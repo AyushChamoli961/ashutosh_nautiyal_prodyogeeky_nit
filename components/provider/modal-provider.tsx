6fc15477cd6e533399c1ad04e4ad9cd2a2f199a5
AyushChamoli961 <ayushchamoli001@gmail.com>
2024-03-10 19:30:18 +0530

    done

--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -2,9 +2,8 @@
 
 import { useEffect, useState } from "react"
 import { CreateClubModal } from "../modals/create-club";
-import { SelectClub } from "../ui/select-club";
-
 import { CreateEvent } from "../modals/create-event";
+
 
 export const ModalProvider = () => {
     const [isMounted,  setIsMounted] = useState(false);
