1a6379831f1f0a3f018c541d1d0bd534b9d239c6
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-03-10 12:35:43 +0530

    frontend done

---
+++ app/components/Header/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="bg-white dark:bg-gray-900">
+      <nav className="bg-white dark:bg-gray-900">
+        <div className="container flex flex-col items-center p-6 mx-auto">
+          <div className="flex items-center justify-center mt-6 text-gray-600 capitalize dark:text-gray-300">
+            <a
+              href="/"
+              className="mx-2 text-gray-800 border-b-2 border-blue-500 dark:text-gray-200 sm:mx-6"
+            >
+              home
+            </a>
+
+            <a
+              href="/about"
+              className="mx-2 border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 sm:mx-6"
+            >
+              about
+            </a>
+            <a
+              href="/contact"
+              className="mx-2 border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 sm:mx-6"
+            >
+              contact
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
